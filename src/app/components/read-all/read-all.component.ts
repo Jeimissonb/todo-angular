3b84647844ee004444ed0cdf19ddf395f6544e43
Jeimissonb <jeimissonb@hotmail.com>
2021-06-01 14:17:08 -0300

    Ajustes no badge para listar apenas os todos em aberto, e contabilizar no badge os já finalizados

--- src/app/components/read-all/read-all.component.ts
+++ src/app/components/read-all/read-all.component.ts
@@ -11,6 +11,7 @@ export class ReadAllComponent implements OnInit {
   closed = 0;
 
   list: Todo [] = [];
+  listFinished: Todo [] = [];
   
   constructor (private service: TodoService){ }
 
@@ -21,21 +22,15 @@ export class ReadAllComponent implements OnInit {
 
   findAll(): void {
     this.service.findAll().subscribe((resposta) =>{
-    this.list = resposta;
-    this.countClosed();
+    resposta.forEach (todo => {
+      if (todo.finalizado){
+        this.listFinished.push(todo);
+      } else {
+        this.list.push(todo);
+      }
+    })
+    this.closed = this.listFinished.length;
     })
   }
 
-  countClosed(): void {
-
-    for(let todo of this.list) {
-      if (todo.finalizado){
-       this.closed++	
-      }
-    }
-
-
-
- 
-  }
 }
